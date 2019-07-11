746e8d2155b412f09e6a64a492b2af7b6bbede87
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-07-12 08:31:23 +0900

    コルーチンを待つのに時間を指定するのは現実的じゃないのでjoinを使う

---
+++ src/main/kotlin/coroutine/ex_3_join.kt
@@ -0,0 +1,15 @@
+package coroutine
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main(args: Array<String>) = runBlocking {
+    val job = GlobalScope.launch {
+        delay(1000L)
+        println("World!")
+    }
+    println("Hello,")
+    job.join()
+}
