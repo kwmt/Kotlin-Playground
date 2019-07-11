6f17163ffec4a4586591a7f117dbfd5e10aa4ddd
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-07-12 08:23:18 +0900

    ex1launch

--- src/main/kotlin/coroutine/ex_2_runBlocking.kt
+++ src/main/kotlin/coroutine/ex_2_runBlocking.kt
@@ -3,10 +3,17 @@ package coroutine
 import kotlinx.coroutines.*
 
 fun main(args: Array<String>) {
-    GlobalScope.launch { // lancement de la coroutine
+    println("Start")
+
+    GlobalScope.launch {
+        // lancement de la coroutine
         delay(1000L) // suspension de la coroutine pendant 1 seconde
         println("World!")
     }
     println("Hello,") // le thread principal continue tant que la coroutine est suspendue
-    Thread.sleep(2000L) // suspension du thread principal pendant 2 secondes pour conserver la JVM en exécution (pour attendre la fin de la coroutine)
+    runBlocking {
+        delay(2000L) // suspension de la coroutine pendant 1 seconde
+        println("runblockin!")
+    }
+
 }
