20651f8c09101b0b0a50e8022b12d2af286854e0
Yasutaka Kawamoto <kwmt27@gmail.com>
2021-01-13 12:33:38 +0900

    ordinal enumValues

--- src/main/kotlin/enum/ordinal.kt
+++ src/main/kotlin/enum/ordinal.kt
@@ -3,6 +3,9 @@ package enum
 fun main() {
     println("Item.ordinal:${Type.Item.ordinal}")
     println("Footer.ordinal:${Type.Footer.ordinal}")
+
+    val v = enumValues<Type>()
+    println(v)
 }
 
 enum class Type {
