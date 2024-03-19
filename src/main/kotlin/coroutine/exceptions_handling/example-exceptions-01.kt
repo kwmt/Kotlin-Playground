a77b196f5786e7bd7e584959f2c1aa0409b48413
Yasutaka.Kawamoto <yasutaka.kawamoto@appbrew.io>
2024-03-19 12:15:50 +0900

    launchをtry catch

--- src/main/kotlin/coroutine/exceptions_handling/example-exceptions-01.kt
+++ src/main/kotlin/coroutine/exceptions_handling/example-exceptions-01.kt
@@ -4,11 +4,16 @@ import kotlinx.coroutines.*
 
 @OptIn(DelicateCoroutinesApi::class)
 fun main() = runBlocking {
-    val job = GlobalScope.launch {// root coroutine with launch
-        println("Throwing exception from launch")
-        throw IndexOutOfBoundsException()  // Will be printed to the console by Thread.defaultUncaughtExceptionHandler
+    var job: Job? = null
+    try {
+        job = GlobalScope.launch {// root coroutine with launch
+            println("Throwing exception from launch")
+            throw IndexOutOfBoundsException()  // Will be printed to the console by Thread.defaultUncaughtExceptionHandler
+        }
+    } catch (e: IndexOutOfBoundsException) {
+        println("Caught IndexOutOfBoundsException")
     }
-    job.join()
+    job?.join()
     println("Joined failed job")
     val deferred = GlobalScope.async {
         println("Throwing exception from async")
